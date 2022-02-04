de012163cb67bd4527b5b990973c1fcdd161938b
FirmanAzharR <firmanazharr@gmail.com>
2022-02-04 14:40:20 +0700

    Added a Procfile.

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const app = express();
 const port = 3000;
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello server is running").end();
+});
+
 app.listen(port, () => {
   console.log(`app is running on port ${port}`);
 });
