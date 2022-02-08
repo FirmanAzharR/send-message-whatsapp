f05be0ac42d7147e03ca2ec4a09364a11d46db4c
FirmanAzharR <firmanazharr@gmail.com>
2022-02-08 16:47:33 +0700

    add send message with whatsapp-web.js

--- index.js
+++ index.js
@@ -1,9 +1,61 @@
 const express = require("express");
+const { Client } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
+const bodyParser = require("body-parser");
+const fs = require("fs");
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.status(200).send("Hello server is running").end();
+});
+
+app.post("/whatsapp/send", (req, res) => {
+  try {
+    const number = req.body.number;
+    const SESSION_FILE_PATH = "./session.json";
+    let sessionData;
+
+    if (fs.existsSync(SESSION_FILE_PATH)) {
+      sessionData = require(SESSION_FILE_PATH);
+    }
+
+    const client = new Client({
+      session: sessionData,
+    });
+
+    client.on("qr", (qr) => {
+      qrcode.generate(qr, { small: true });
+      console.log("QR RECEIVED", qr);
+    });
+
+    client.on("authenticated", (session) => {
+      sessionData = session;
+      fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
+        if (err) {
+          console.error(err);
+        }
+      });
+    });
+
+    client.on("ready", async () => {
+      const text = "Your OTP 749381";
+      // we have to delete "+" from the beginning and add "@c.us" at the end of the number.
+      const chatId = number.substring(1) + "@c.us";
+      // Sending message.
+      await client
+        .sendMessage(chatId, text)
+        .then(() => res.send("success-send-message"))
+        .catch((e) => res.send(e));
+    });
+
+    client.initialize();
+  } catch (e) {
+    //fs.unlink("./session.json");
+    console.log(e);
+  }
 });
 
 app.listen(process.env.PORT || 8080, () => {
